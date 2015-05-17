2f6c89fdaee65669d2bbaf46b2ce9e1e690115fd
NivedhithaV <nivedhi170589@gmail.com>
2015-05-16 20:05:10 -0700

    Changes made to chroniclehashmap

--- server/src/main/java/edu/sjsu/cmpe/cache/CacheService.java
+++ server/src/main/java/edu/sjsu/cmpe/cache/CacheService.java
@@ -3,19 +3,31 @@ package edu.sjsu.cmpe.cache;
 import com.yammer.dropwizard.Service;
 import com.yammer.dropwizard.config.Bootstrap;
 import com.yammer.dropwizard.config.Environment;
+
 import edu.sjsu.cmpe.cache.api.resources.CacheResource;
 import edu.sjsu.cmpe.cache.config.CacheServiceConfiguration;
+import edu.sjsu.cmpe.cache.domain.Entry;
 import edu.sjsu.cmpe.cache.repository.CacheInterface;
 import edu.sjsu.cmpe.cache.repository.ChronicleMapCache;
+import java.io.File;
+
+import net.openhft.chronicle.map.ChronicleMap;
+import net.openhft.chronicle.map.ChronicleMapBuilder;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+
 
 public class CacheService extends Service<CacheServiceConfiguration> {
 
     private final Logger log = LoggerFactory.getLogger(getClass());
+    private String fileName = null;
 
     public static void main(String[] args) throws Exception {
-        new CacheService().run(args);
+        CacheService cacheService = new CacheService();
+        cacheService.setFileName("cache_" + args[1].substring(7, 15) + ".txt");
+        cacheService.run(args);
     }
 
     @Override
@@ -26,11 +38,20 @@ public class CacheService extends Service<CacheServiceConfiguration> {
     @Override
     public void run(CacheServiceConfiguration configuration,
             Environment environment) throws Exception {
-        /** Cache APIs */
-    	// using chronicle hash map instead of in memory cache
-        CacheInterface cache = new ChronicleMapCache();
+        /** Cache APIs */       
+        File file = new File(getFileName());        
+        ChronicleMap<Long, Entry> map = ChronicleMapBuilder
+                .of(Long.class, Entry.class).createPersistedTo(file);
+        CacheInterface cache = new ChronicleMapCache(map);
         environment.addResource(new CacheResource(cache));
         log.info("Loaded resources");
+    }
 
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
     }
 }
--- server/src/main/java/edu/sjsu/cmpe/cache/repository/ChronicleMapCache.java
+++ server/src/main/java/edu/sjsu/cmpe/cache/repository/ChronicleMapCache.java
@@ -3,44 +3,36 @@ package edu.sjsu.cmpe.cache.repository;
 import edu.sjsu.cmpe.cache.domain.Entry;
 import net.openhft.chronicle.map.ChronicleMapBuilder;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentMap;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 public class ChronicleMapCache implements CacheInterface {
 
-    private final Map<Long, Entry> chronicleMap;
+    private final ChronicleMap<Long, Entry> chronicleMapCache;
 
-    public ChronicleMapCache() throws IOException {
-
-        String pathname ="serverbindatafile.dat";
-        File file=new File(pathname);
-        Map<Long, Entry> map= ChronicleMapBuilder.of(Long.class, Entry.class).createPersistedTo(file);
-        chronicleMap=map;
+    public ChronicleMapCache(ChronicleMap<Long, Entry> cacheMap) {
+        chronicleMapCache = cacheMap;
     }
+
     @Override
     public Entry save(Entry newEntry) {
-        checkNotNull(newEntry, "newEntry instance should not be null");
-        ((ConcurrentMap<Long, Entry>) chronicleMap).putIfAbsent(newEntry.getKey(),newEntry);
+        checkNotNull(newEntry, "newEntry instance must not be null");
+        chronicleMapCache.putIfAbsent(newEntry.getKey(), newEntry);
+
         return newEntry;
     }
 
     @Override
     public Entry get(Long key) {
-        checkArgument(key > 0,
-                "Key was %s but expected greater than zero value", key);
-        //Entry using=new Entry();
-        return chronicleMap.get(key);
+        checkArgument(key > 0, "Key was %s but expected greater than zero value", key);
+        return chronicleMapCache.get(key);
     }
 
     @Override
     public List<Entry> getAll() {
-        return new ArrayList<Entry>(chronicleMap.values());
+        return new ArrayList<Entry>(chronicleMapCache.values());
     }
 }
